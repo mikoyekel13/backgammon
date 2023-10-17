ec74f53534e5110127273d88924fc72cde705096
mikoyekel13 <mikoyekel@hilma.tech>
2023-10-17 16:04:15 +0300

    pieces movement

--- JS/backgammonJS.js
+++ JS/backgammonJS.js
@@ -28,20 +28,19 @@ function startGame() {
     addGamePiece(containers[5], 'black', 5);
     addGamePiece(containers[7], 'black', 3);
     addGamePiece(containers[12], 'black', 5);
-    turn('black');
+    turn('white');
 }
 
 //turn
 const diceResult = document.querySelector('#diceResult');
-let diceResults;
+let diceResults; let possible; let nowPossible;
 
 function turn(color) {
     //roll dice
     diceResults = diceRoll();
-    //find your pieces
-    markPieces(color, containers);
-    //find possible places
-    //choose place
+    //move as many times as allowed
+    possible = findPossibe();
+    markPieces(color);
     //reset or confirm
 }
 
@@ -53,78 +52,160 @@ function diceRoll() {
     return [result1, result2];
 }
 
-function markPieces(color, containers) {
+function markPieces(color) {
+    resetColor();
     for (let item of containers) {
         if (item.innerHTML.includes('whiteGamePiece') && color === 'white') {
             item.style.background = 'rgb(0, 247, 255, 50)';
-            item.addEventListener('click', findPossible);
+            item.addEventListener('click', lightPossible);
         } else if (item.innerHTML.includes('blackGamePiece') && color === 'black') {
             item.style.background = 'rgb(0, 247, 255, 50)';
-            item.addEventListener('click', findPossible);
-        } else {
-            item.removeEventListener('click', findPossible);
+            item.addEventListener('click', lightPossible);
         }
     }
 }
 
-function findPossible() {
-    let searchedKey;
-    for (let i = 0; i < containers.length; i++) {
-        containers[i].style.background = 'rgb(228, 184, 126)';
-        if (containers[i].id === this.id) {
-            searchedKey = i;
-        }
-    }
+function lightPossible() {
+    resetColor();
     this.style.background = 'rgb(43, 255, 0)';
     if (this.innerHTML.includes('whiteGamePiece')) {
-        let possibility1 = searchedKey + diceResults[0];
-        let possibility2 = searchedKey + diceResults[1];
-        let possibility3 = searchedKey + diceResults[0] + diceResults[1];
-        if (possibility1 <= 23 && !(containers[possibility1].innerHTML.includes('blackGamePiece') && containers[possibility1].children.length >= 2)){
-            containers[possibility1].style.background = 'rgb(0, 247, 255, 50)';
-        } if (possibility2 <= 23 && !(containers[possibility2].innerHTML.includes('blackGamePiece') && containers[possibility2].children.length >= 2)) {
-            containers[possibility2].style.background = 'rgb(0, 247, 255, 50)';
-        } if (possibility3 <= 23 && !(containers[possibility3].innerHTML.includes('blackGamePiece') && containers[possibility3].children.length >= 2)) {
-            containers[possibility3].style.background = 'rgb(0, 247, 255, 50)';
+        nowPossible = findPossibeWhite(this);
+        for (let item of nowPossible) {
+            if (item <= 23 && !(containers[item].innerHTML.includes('blackGamePiece') && containers[item].children.length >= 2)) {
+                containers[item].style.background = 'rgb(0, 247, 255, 50)';
+                containers[item].removeEventListener('click', lightPossible);
+                containers[item].addEventListener('click', movePieceHere);
+            }
         }
     } else if (this.innerHTML.includes('blackGamePiece')) {
-        let possibility1 = searchedKey - diceResults[0];
-        let possibility2 = searchedKey - diceResults[1];
-        let possibility3 = searchedKey - diceResults[0] - diceResults[1];
-        if (possibility1 <= 23 && !(containers[possibility1].innerHTML.includes('whiteGamePiece') && containers[possibility1].children.length >= 2)){
-            containers[possibility1].style.background = 'rgb(0, 247, 255, 50)';
-        } if (possibility2 <= 23 && !(containers[possibility2].innerHTML.includes('whiteGamePiece') && containers[possibility2].children.length >= 2)) {
-            containers[possibility2].style.background = 'rgb(0, 247, 255, 50)';
-        } if (possibility3 <= 23 && !(containers[possibility3].innerHTML.includes('whiteGamePiece') && containers[possibility3].children.length >= 2)) {
-            containers[possibility3].style.background = 'rgb(0, 247, 255, 50)';
+        nowPossible = findPossibeBlack(this);
+        for (let item of nowPossible) {
+            if (item <= 23 && !(containers[item].innerHTML.includes('whiteGamePiece') && containers[item].children.length >= 2)) {
+                containers[item].style.background = 'rgb(0, 247, 255, 50)';
+                containers[item].removeEventListener('click', lightPossible);
+                containers[item].addEventListener('click', movePieceHere);
+            }
         }
     }
+}
+
+function findPossibe() {
+    if (diceResults[0] === diceResults[1]) {
+        return [
+            diceResults[0],
+            (2 * diceResults[0]),
+            (3 * diceResults[0]),
+            (4 * diceResults[0]),
+        ];
+    } else {
+        return [
+            diceResults[0],
+            diceResults[1],
+            diceResults[0] + diceResults[1],
+        ]
+    }
+}
+
+function findPossibeWhite(container) {
+    let searchedKey = findKey(container);
+    return possible.map(item => searchedKey + item);
+}
+
+function findPossibeBlack(container) {
+    let searchedKey = findKey(container);
+    return possible.map(item => searchedKey - item);
 }
 
 function addGamePiece(container, color, num = 1) {
     if (color === 'white') {
         for (let i = 0; i < num; i++) {
-            if (container.children.length < 4) {
-                container.innerHTML += '<div class="whiteGamePiece"></div>';
-            } else {
-                break;
-            }
-        } if (num - 4 == 1) {
             container.innerHTML += '<div class="whiteGamePiece"></div>';
-        } else if (num - 4 > 1) {
-            container.innerHTML += `<div class="whiteGamePiece">${num - 4}</div>`;
         }
     } else if (color === 'black') {
         for (let i = 0; i < num; i++) {
-            if (container.children.length < 4) {
-                container.innerHTML += '<div class="blackGamePiece"></div>';
-            } else {
-                break;
-            }
-        } if (num - 4 == 1) {
             container.innerHTML += '<div class="blackGamePiece"></div>';
-        } else if (num - 4 > 1) {
-            container.innerHTML += `<div class="blackGamePiece">${num - 4}</div>`;
         }
+    }
+    if (container.children.length > 5) {
+        container.children[4].innerHTML = `+${container.children.length - 5}`;
+    }
+}
+
+function removeGamePiece(container) {
+    container.removeChild(container.lastChild);
+    if (container.children.length > 5) {
+        container.children[4].innerHTML = `+${container.children.length - 5}`;
+    }
+}
+
+function movePieceHere() {
+    let color;
+    handlePossible(this);
+    for (let i = 0; i < containers.length; i++) {
+        if (containers[i].style.background == 'rgb(43, 255, 0)') {
+            if (containers[i].innerHTML.includes('whiteGamePiece')) {
+                color = 'white';
+            } else if (containers[i].innerHTML.includes('blackGamePiece')) {
+                color = 'black';
+            }
+            removeGamePiece(containers[i]);
+            break;
+        }
+    }
+    addGamePiece(this, color);
+    resetColor();
+    resetEventListeners();
+    this.style.background = 'rgb(255, 98, 171)';
+    if (possible.length > 0) {
+        markPieces(color);
+    } else {
+        turn(oppositeColor(color));
+    }
+}
+
+function handlePossible(container) {
+    if (container === containers[nowPossible[3]]) {
+        possible.splice(-4);
+    } else if (container === containers[nowPossible[2]]) {
+        possible.splice(-3);
+    } else if (container === containers[nowPossible[1]]) {
+        possible.splice(-2);
+    } else if (container === containers[nowPossible[0]] && diceResults[0] == diceResults[1]) {
+        possible.splice(-1);
+    } else if (container === containers[nowPossible[0]] && diceResults[0] != diceResults[1]) {
+        possible = [possible[1]];
+    } 
+    if (typeof possible[0] == 'undefined') {
+        possible = [];
+    }
+}
+
+function resetColor() {
+    for (let i = 0; i < containers.length; i++) {
+        containers[i].style.background = 'rgb(228, 184, 126)';
+    }
+}
+
+function resetEventListeners() {
+    for (let i = 0; i < containers.length; i++) {
+        containers[i].removeEventListener('click', lightPossible);
+        containers[i].removeEventListener('click', movePieceHere);
+    }
+
+}
+
+function findKey(container) {
+    for (let i = 0; i < containers.length; i++) {
+        if (containers[i].id === container.id) {
+            return i;
+        }
+    }
+}
+
+function oppositeColor(color) {
+    if (color === 'white') {
+        return 'black';
+    } else if (color === 'black') {
+        return 'white';
     }
 }
